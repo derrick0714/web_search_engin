82a8d371f6e78bf4382ab0f4d6dbb06c44f95069
derrick0714 <derrick0714@gmail.com>
2013-02-01 18:12:41 -0500

    modify readme

---
+++ crawling/include/strategies.py
@@ -0,0 +1,20 @@
+from include.misc import load_object
+
+class CStrategie(object):
+	"""docstring for CStrategie"""
+	def __init__(self, arg):
+	
+		
+
+
+class CStrategiesManager(object):
+	"""docstring for CStrategies"""
+	def __init__(self, arg):
+		
+	def load( self ):
+
+	def count( self ):
+
+	def get_status( self, strategie_name )
+
+	
