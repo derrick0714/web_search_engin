2a011edc8d6a73a4948eb4d7f99250a9c8cb034c
Adam57 <edwinj619@gmail.com>
2013-02-02 11:48:04 -0500

    log class input to file
    log class input to file

--- crawling/include/log.py
+++ crawling/include/log.py
@@ -1,6 +1,49 @@
+"""
+Created on Feb 1, 2013
 
-class Log(object):
+@author: Adam57
 
-	def write(self, string):
-		print(string)
-		
+This part takes care of the logging
+"""
+import os 
+import time 
+import logging 
+import inspect
+
+class Log(object):	 
+    def __init__(self): 
+        self.__logger = logging.getLogger() 
+        path = os.path.abspath("/Log-error.log") 
+        handler=logging.FileHandler(path) 
+        self.__logger.addHandler(handler) 
+        self.__logger.setLevel(logging.NOTSET)
+    def getLogMessage(self,level,message):
+        filename,lineNo,functionName = inspect.stack()[2]
+        return "[%s] [%s] [%s - %s - %s] %s" %(self.printfNow(),level,filename,lineNo,functionName,message)
+    def info(self,message): 
+        message = self.getLogMessage("info",message) 
+        self.__logger.info(message)
+    def error(self,message): 
+        message = self.getLogMessage("error",message)
+        self.__logger.error(message)
+    def warning(self,message): 
+        message = self.getLogMessage("warning",message) 
+        self.__logger.warning(message)
+    def debug(self,message): 
+        message = self.getLogMessage("debug",message) 
+        self.__logger.debug(message)
+    def critical(self,message): 
+        message = self.getLogMessage("critical",message) 
+        self.__logger.critical(message)
+    def printfNow(self): 
+        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    def write(self, string):
+        print(string)	
+
+
+if __name__ == "__main__": 
+    logger = Log() 
+    logger.info("hello")
+    logger.warning("hello")
+    print("wait a sec")
+
