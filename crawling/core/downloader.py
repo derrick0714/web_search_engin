7be4bcb90a00092b39933f0a20cab8987cbbf90d
derrick0714 <derrick0714@gmail.com>
2013-02-03 12:53:27 -0500

    initial commit downloader

---
+++ crawling/core/downloader.py
@@ -0,0 +1,41 @@
+"""
+Created on Feb 2, 2013
+
+@author: derrick
+
+This is a downloader
+"""
+
+
+from include.setting import setting
+from include.thread_pool import ThreadPool
+from include.log import Log
+import urllib.request
+import urllib.parse
+
+
+class Downloader:
+	def __init__(self, setting):
+	    self.__num_threads   = 3
+	    self.__workers_pool	 = ThreadPool(self.__num_threads)
+	    
+
+	def download(self, resoure):
+		__workers_pool.start()
+	def stop(self):
+		__workers_pool.start()
+
+	def download_one(sefl, url):
+		req = urllib.request.Request(url)
+		data = urllib.request.urlopen(req)
+		return data
+
+	def get_pages(self):
+		pass
+
+
+#test
+if __name__ == "__main__":
+
+  downloader = Downloader(setting())
+  print(downloader.download())
