b17259e2f78a2ee1b768f3a1c4d33347afcf1c3e
Adam57 <edwinj619@gmail.com>
2013-02-03 13:33:07 -0500

    modify setting

--- crawling/include/setting.py
+++ crawling/include/setting.py
@@ -7,28 +7,37 @@ import configparser
 
 class setting(object):
               
-    def get_strategies(self):
+    def get_section(self, section):
         conf = configparser.ConfigParser()
         conf.read("c:\\config.ini")
-        return conf.items("strategies")
+        return conf.items(section)
     
-    def get_log(self):
+    def get_param(self, section, param):
         conf = configparser.ConfigParser()
         conf.read("c:\\config.ini")
-        return conf.items("log")
+        return conf.get(section,param)
     
-    def get_thread(self):
+    def set_param(self, section, param, new_value):
         conf = configparser.ConfigParser()
         conf.read("c:\\config.ini")
-        return conf.items("thread")
-    
-    def get_queue(self):
+        conf.set(section, param, new_value)
+        conf.write(open("c:\\config.ini", "w"))
+        
+    def add_param(self, section, new_param, new_value):
         conf = configparser.ConfigParser()
         conf.read("c:\\config.ini")
-        return conf.items("queue")
+        conf.set(section, new_param, new_value)
+        conf.write(open("c:\\config.ini", "w"))   
+
     
 if __name__ == "__main__": 
     config = setting() 
-    print (config.get_log())
+    print (config.get_section("log"))
+    config.set_param("log", "L1", "TEST")   
+    print (config.get_section("log"))
+    config.add_param("log","L4", "new") 
+    print (config.get_section("log"))    
+    print(config.get_param("strategies","S1"))
+    
     
         
