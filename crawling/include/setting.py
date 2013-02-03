837437f22cc5fd6feee300085dc7a51f927627e5
Adam57 <edwinj619@gmail.com>
2013-02-03 13:41:54 -0500

    modify setting again

--- crawling/include/setting.py
+++ crawling/include/setting.py
@@ -5,39 +5,44 @@ Created on Feb 2, 2013
 '''
 import configparser
 
-class setting(object):
+class setting(object):   
+    
+    def load(self, path):
+        self.path = path 
               
     def get_section(self, section):
         conf = configparser.ConfigParser()
-        conf.read("c:\\config.ini")
+        conf.read(self.path)
         return conf.items(section)
     
     def get_param(self, section, param):
         conf = configparser.ConfigParser()
-        conf.read("c:\\config.ini")
+        conf.read(self.path)
         return conf.get(section,param)
     
     def set_param(self, section, param, new_value):
         conf = configparser.ConfigParser()
-        conf.read("c:\\config.ini")
+        conf.read(self.path)
         conf.set(section, param, new_value)
-        conf.write(open("c:\\config.ini", "w"))
+        conf.write(open(self.path, "w"))
         
     def add_param(self, section, new_param, new_value):
         conf = configparser.ConfigParser()
-        conf.read("c:\\config.ini")
+        conf.read(self.path)
         conf.set(section, new_param, new_value)
-        conf.write(open("c:\\config.ini", "w"))   
+        conf.write(open(self.path, "w"))  
+#"c:\\config.ini" 
 
     
 if __name__ == "__main__": 
     config = setting() 
+    config.load("c:\\config.ini" );
     print (config.get_section("log"))
-    config.set_param("log", "L1", "TEST")   
+    config.set_param("log", "L1", "trial")   
     print (config.get_section("log"))
-    config.add_param("log","L4", "new") 
+    config.add_param("log","L4", "new trial") 
     print (config.get_section("log"))    
-    print(config.get_param("strategies","S1"))
+    print(config.get_param("log","L4"))
     
     
         
