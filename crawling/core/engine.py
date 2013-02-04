5dcec1caba333d3bbc71fc4dde300662debde4ef
derrick0714 <derrick0714@gmail.com>
2013-02-04 14:40:31 -0500

    make a basic work flow

--- crawling/core/engine.py
+++ crawling/core/engine.py
@@ -9,23 +9,33 @@ Created on Feb 2, 2013
 	    
 from include import log, setting, status
 from core.downloader import Downloader
+from core.parse import Parse
+from queue import deque
 
 class Engine(object):
 	def __init__( self, setting ):
-		self._seed 		= []
-		self._stauts	= Status()
-		
+		self._seed 			= []
+		#self._stauts		= Status()
+		self._downloader	= Downloader( int(setting.get_param("Downloader","Threadnum")) )
+		self._parse			= Parse()
+		self._url_pool		= deque()
 
-
+	def add_seed( self, url ):
+		self._seed.append(url)
+		self._downloader.queue_download_task( url, self.finish_download)
 	def start(self):
-		pass
+		self._downloader.start()
+		self._parse.start()
 
 	def stop(self):
-		pass
+		self._downloader.stop()
+		self._parse.stop()
 
 	def pause(self):
 		pass
 
-	def add_seed( self, seed ):
-		pass
+	def finish_download(self, data):
+		print("finish download")
+
+
 		
