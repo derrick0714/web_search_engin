193db9b77491ea0ee1645d2fc1bd42f1e1cae499
Adam57 <edwinj619@gmail.com>
2013-02-04 11:31:51 -0500

    modify download

--- crawling/core/downloader.py
+++ crawling/core/downloader.py
@@ -16,16 +16,16 @@ import urllib.parse
 
 class Downloader:
 	def __init__(self, setting):
-	    self.__num_threads   = 3
-	    self.__workers_pool	 = ThreadPool(self.__num_threads)
-	    
-
+		self.__num_threads   = 3
+		self.__task_queue_max = 10
+		self.__workers_pool	 = ThreadPool(self.__num_threads,self.__task_queue_max)
+		
 	def download(self, resoure):
-		__workers_pool.start()
+		self.__workers_pool.start()
 	def stop(self):
-		__workers_pool.start()
+		self.__workers_pool.start()
 
-	def download_one(sefl, url):
+	def download_one(self, url):
 		req = urllib.request.Request(url)
 		data = urllib.request.urlopen(req)
 		return data
@@ -36,6 +36,5 @@ class Downloader:
 
 #test
 if __name__ == "__main__":
-
-  downloader = Downloader(setting())
-  print(downloader.download())
+	downloader = Downloader(setting())
+	print(downloader.download_one("http://cis.poly.edu/cs6913/"))
