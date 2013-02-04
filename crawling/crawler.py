53ff70b9ec5e62986862dd127627b410706b946f
derrick0714 <derrick0714@gmail.com>
2013-02-04 14:40:54 -0500

    make a demo

--- crawling/crawler.py
+++ crawling/crawler.py
@@ -1,11 +1,31 @@
 from core.engine import Engine
 from core.downloader import Downloader
-from include.setting import setting
+from include.setting import Setting
+from time import sleep
+
 
 def main():
-	a = Downloader(setting())
-	#print(a.download_one("http://dengxu.me").read().decode('utf-8'))
-	print (a.download_one("http://dengxu.me"))
+	#load setting
+	setting = Setting()
+	setting.load("config.ini")
+	
+	#create crawler engin
+	crawler_engine = Engine(setting)
+
+	
+	#start engine 
+	crawler_engine.start( )
+
+	#load seed info from config file
+	crawler_engine.add_seed(setting.get_param('seed','first') )
+
+
+
+	sleep(1)
+	
+	input('press any key to exit')
+	#stop engin 
+	crawler_engine.stop()
 	
 if __name__ == "__main__":
     main()
