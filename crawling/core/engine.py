6343b7a7ab2841962c701240c3b202e2e61e7ec3
derrick0714 <derrick0714@gmail.com>
2013-02-05 14:57:24 -0500

    add checker thread to monitor the downloader pool and parse pool

--- crawling/core/engine.py
+++ crawling/core/engine.py
@@ -6,35 +6,49 @@ Created on Feb 2, 2013
 
 """
 	    
-	    
+from threading import Thread	    
 from include import log, setting, status
 from core.downloader import Downloader
 from core.parse import Parse
-from queue import deque
-from models.html import Html 
-from time import time
+from models.html import Html
+from models.safe_queue import SafeQueue
+from time import time, sleep
 
 class Engine(object):
 	def __init__( self, setting ):
 		self._seed 			= []
+		self._istart		= False
 		#self._stauts		= Status()
 		self._downloader	= Downloader( int(setting.get_param("Downloader","Threadnum")) )
 		self._parse			= Parse()
-		self._url_pool		= deque()
+		self._download_pool	= SafeQueue()
+		self._prase_pool	= SafeQueue()
 		self.start_time		= time() # for 
 		self.download_times	= 0 # for test
 
+		self._downloader_pool_checker = Thread( target=self.download_pool_checker )
+		self._prase_pool_cheker = Thread( target=self.prase_pool_cheker)
+		
 
 	def add_seed( self, url ):
 		self._seed.append(url)
-		self._downloader.queue_download_task( url, self.finish_download)
+		html_task = Html(url)
+		self._download_pool.append( html_task )
+		
 	def start(self):
+		self._istart = True
 		self._downloader.start()
 		self._parse.start()
+		self._downloader_pool_checker.start()
+		self._prase_pool_cheker.start()
+
 
 	def stop(self):
+		self._istart = False
 		self._downloader.stop()
 		self._parse.stop()
+		self._downloader_pool_checker.join()
+		self._prase_pool_cheker.join()
 
 	def pause(self):
 		pass
@@ -42,8 +56,28 @@ class Engine(object):
 	def finish_download(self, html):
 		self.download_times+=1
 		print("finish download:{0} {1}".format(self.download_times, time()-self.start_time))
+
+	def finish_parse(self, html):
+		#self.download_times+=1
+		print("finish parse")
 		
 	
+	
+	def download_pool_checker(self):
+		while ( self._istart == True):
+			new_download_task = self._download_pool.pop_left()
+			if ( new_download_task == None):
+				sleep(0.1)
+			else:
+				self._downloader.queue_download_task( new_download_task , self.finish_download)
+
+	def prase_pool_cheker(self):
+		while ( self._istart == True):
+			new_parse_task = self._download_pool.pop_left()
+			if ( new_parse_task == None):
+				sleep(0.1)
+			else:
+				self._parse.queue_parse_task( new_parse_task._url , self.finish_parse)
 
 		
 		#print(html._data)
--- crawling/crawler.py
+++ crawling/crawler.py
@@ -1,6 +1,6 @@
 from core.engine import Engine
-from core.downloader import Downloader
 from include.setting import Setting
+#from include.commond import Commond
 from time import sleep
 
 
@@ -8,7 +8,25 @@ def main():
 	#load setting
 	setting = Setting()
 	setting.load("config.ini")
-	
+
+	#start commond view
+	"""
+	try:
+		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -i <inputfile> -o <outputfile>'
+			sys.exit()
+      	elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+	print 'Input file is "', inputfile
+	print 'Output file is "', outputfile
+"""
 	#create crawler engin
 	crawler_engine = Engine(setting)
 
@@ -17,16 +35,16 @@ def main():
 	crawler_engine.start( )
 
 	#load seed info from config file
-	for i in range(100):
+	for i in range(10):
 		crawler_engine.add_seed(setting.get_param('seed','first') )
 
 
 
-	sleep(1)
 	
 	input('press any key to exit')
 	#stop engin 
 	crawler_engine.stop()
 	
 if __name__ == "__main__":
+    #main(sys.argv[1:])
     main()
