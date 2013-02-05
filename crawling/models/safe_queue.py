c78c479919904731d99a093d02719b33c465d7f4
derrick0714 <derrick0714@gmail.com>
2013-02-05 14:55:07 -0500

    thread safe queue

---
+++ crawling/models/safe_queue.py
@@ -0,0 +1,32 @@
+"""
+Created on Feb 5, 2013
+
+@author: derrick
+
+"""
+from queue import deque
+from threading import Condition
+from threading import Lock
+
+class SafeQueue( object ):
+	def __init__(self):
+		self._data_queue = deque()
+		self._lock  = Condition(Lock())
+
+
+	def append(self, data ):
+		self._lock.acquire()
+		try:
+			self._data_queue.append( data )
+		finally:
+			self._lock.release()
+		
+
+	def pop_left(self):
+		self._lock.acquire()
+		try:
+			if ( len(self._data_queue) == 0 ):
+				return None
+			return self._data_queue.popleft()
+		finally:
+			self._lock.release()
