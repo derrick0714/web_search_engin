cc101019ce955f040337efc565fcb660fbc74cbf
Adam57 <edwinj619@gmail.com>
2013-02-06 14:48:07 -0500

    correct some typo

--- crawling/core/downloader.py
+++ crawling/core/downloader.py
@@ -9,15 +9,15 @@ This is a downloader
 from models.html import Html 
 from include.thread_pool import ThreadPool
 from include.log import Log
+from include.setting import Setting
 import urllib.request
 import urllib.parse
 
 
 class Downloader(object):
 	def __init__(self, num_thread):
-	    self.__num_threads	  	 = num_thread
-	    self._download_workers	 = ThreadPool(num_thread)
-	    
+		self.__num_threads	  	 = num_thread
+		self._download_workers	 = ThreadPool(num_thread)
 
 	def queue_download_task(self, html_task , callback):
 
@@ -26,23 +26,22 @@ class Downloader(object):
 		#callback( result )
 
 	def start(self):
-		 self._download_workers.start()
+		self._download_workers.start()
 
 	def stop(self):
-		 self._download_workers.stop()
+		self._download_workers.stop()
 
-	def download_page(sefl, html_task, callback):
+	def download_page(self, html_task, callback):
 		#print (html_task._url)
 		req = urllib.request.Request(html_task._url)
 		data = urllib.request.urlopen(req)
 		html_task._data = data.read()#.decode('utf-8')
 		#print (html_task._data)
-		callback( html_task )
+		callback(html_task)
 
 
 
 #test
 if __name__ == "__main__":
-
-  downloader = Downloader(setting())
-  print(downloader.download())
+	downloader = Downloader(Setting())
+	print(downloader.download())
