5e339ea9e10d345f98e8d54bab54f23096a503a5
Qi Wang <edwinj619@gmail.com>
2013-02-15 00:07:23 -0500

    Filetypehandler

--- crawling/strategies/filetypehandler.py
+++ crawling/strategies/filetypehandler.py
@@ -3,6 +3,12 @@ Created on Feb 14, 2013
 
 @author: Adam57
 '''
+
+import mimetypes
+
 class FileTypeHandler(object):
-    def FileTypeChecker(self):
-        return True
+    def FileTypeChecker(self,html_task):
+        if mimetypes.guess_type(html_task._url)[0] in ['text/html', None]:
+            return True
+        else:
+            return False
