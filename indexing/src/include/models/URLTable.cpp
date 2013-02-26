42a02abbb6e7fcf26bde042ec38c0e02f1afbf96
Qi Wang <edwinj619@gmail.com>
2013-02-26 03:43:56 -0500

    serialization

--- indexing/src/include/models/URLTable.cpp
+++ indexing/src/include/models/URLTable.cpp
@@ -16,3 +16,40 @@ URLTable::~URLTable() {
 	// TODO Auto-generated destructor stub
 }
 
+void URLTable::serialize( ostream &stream) {
+
+	for ( std::map<string, int>::iterator it = map.begin(); it!=map.end(); ++it){
+			stream.write(reinterpret_cast <const char*> ( it->first), sizeof(it->first));
+			stream.write(reinterpret_cast <const char*> ( it->second), sizeof(it->second));
+	}
+
+}
+
+void URLTable::deserialize( istream &stream ){
+	for (int i=0; i<map.max_size();i++ ){
+		 //std::vector<char> buffer(sizeof())
+		 //need to extract the key and value from the stream, no fixed size, using /0?
+	}
+
+}
+
+stringbuf& URLTable::operator<<(stringbuf &stream){
+	std::istream ibuffer(&stream);
+	deserialize(ibuffer);
+	return stream;
+}
+
+stringbuf& URLTable::operator>>(stringbuf &stream){
+	std::ostream obuffer(&stream);
+	serialize(obuffer);
+	return stream;
+}
+
+ostream& URLTable::operator<<(ostream &stream){
+	stream << "Container {" << std::endl;
+	for ( std::map<string, int>::iterator it = map.begin(); it!=map.end(); ++it){
+		stream << it->first << ": " << it->second << std::endl;
+	}
+	stream << "}" << std::endl << std::endl;
+	stream << "}" << std::endl << std::endl;
+}
--- indexing/src/include/models/URLTable.h
+++ indexing/src/include/models/URLTable.h
@@ -8,15 +8,22 @@
 #ifndef URLTABLE_H_
 #define URLTABLE_H_
 #include <map>
+#include <iostream>
 using namespace std;
 
 class URLTable {
 public:
 	URLTable();
 	virtual ~URLTable();
+	stringbuf& operator<<(stringbuf &stream);
+	stringbuf& operator>>(stringbuf &stream);
+	ostream&   operator<<(ostream &stream);
+
 private:
 	static URLTable instance;
 	map<string, int> map;
+	void serialize( ostream &stream);
+	void deserialize( istream &stream);
 };
 
 #endif /* URLTABLE_H_ */
