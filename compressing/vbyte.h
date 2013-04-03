6f7fa5bb42602b271497f77d7390e0b4de8d8c71
Qi Wang <edwinj619@gmail.com>
2013-04-03 00:24:52 -0400

    compressing 4ops

--- compressing/vbyte.h
+++ compressing/vbyte.h
@@ -1,19 +1,31 @@
-#include "vbyte.h"
+/*
+ * vbyte.h
+ *
+ *  Created on: Apr 2, 2013
+ *      Author: Adam57
+ */
 
-    int writeVbyte(unsigned int ui, unsigned char* buf){
+#ifndef VBYTE_H_
+#define VBYTE_H_
+
+
+int writeVbyte(unsigned int ui, unsigned char* buf){
 
         int len = 0;
 
         while( (ui & ~0x7F) != 0){
+//        	printf("%u, larger than 127\n", ui);
             buf[len++] = ((ui & 0x7F) | 0x80 );
+//            printf("%u, still larger than 127\n", ((ui & 0x7F) | 0x80 ));
             ui >>= 7;
+//            printf("%u, after shifting\n", ui);
         }
         buf[len++] = ui;
 
         return len;
     }
 
-    int readVbyte(unsigned char * buf, unsigned int & res){
+int readVbyte(unsigned char * buf, unsigned int & res){
 
         int pos = 0;
         unsigned char b = buf[pos++];
@@ -28,3 +40,4 @@
         return pos;
     }
 
+#endif /* VBYTE_H_ */
---
+++ query_processing/Lp.cpp
@@ -0,0 +1,18 @@
+/*
+ * Lp.cpp
+ *
+ *  Created on: Mar 31, 2013
+ *      Author: Adam57
+ */
+
+#include "Lp.h"
+
+Lp::Lp() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Lp::~Lp() {
+	// TODO Auto-generated destructor stub
+}
+
